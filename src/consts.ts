d84b2bd238cf52d0b0f50c94797d0f500885294f
Call_Me_DongTeacher <donghanying20120408@outlook.com>
2026-02-15 14:25:07 +0800

    更改联系方式
    仍有bug，404，未知原因

--- src/consts.ts
+++ src/consts.ts
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "donghanying20120408@outlook.com",
+    HREF: "mailto:donghanying20120408@outlook.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "CallMeDongTeacher",
+    HREF: "https://github.com/CallMeDongTeacher/"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "no",
+    HREF: "no",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "no",
+    HREF: "no",
   },
 ]
 
