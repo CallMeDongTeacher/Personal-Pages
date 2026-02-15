e62c073ec4013e1c9801887de45e43ee68882363
Call_Me_DongTeacher <donghanying20120408@outlook.com>
2026-02-15 13:23:43 +0800

    （11）

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import edgeoneAdapter from "@edgeone/astro"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dhy-8yd.pages.dev",
+  output: "static",
+  adapter: edgeoneAdapter(),
+  site: "https://astro-sphere.edgeone.app/",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
